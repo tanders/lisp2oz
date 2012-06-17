a9610380e6a03567d1b276972ef46d51ff07c881
Torsten Anders <torsten.anders@beds.ac.uk>
2012-06-17 16:26:53 +0100

    minor test edit

--- testing/LispWorks-test.lisp
+++ testing/LispWorks-test.lisp
@@ -7,7 +7,7 @@
 
 
 ;; start OzServer in a shell and establish connection
-(run-oz)				
+(run-oz :port 5001)				
 
 ;; alternative way to start OzServer in a shell (alternatively, first run start-oz-server and a bith later then run connect-to-oz)
 ; (start-oz-server :oz-server "~/.oz/1.3.1/bin/OzServer" :port 5010)
@@ -27,5 +27,4 @@
 ;; see file ./general-test.lisp for further tests
 
 (quit-oz)
-
 
